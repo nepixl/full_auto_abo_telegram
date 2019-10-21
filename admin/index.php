a30a19d18d5e37c1522fa0998ae879389dbfc538
pixl <41138223+nepixl@users.noreply.github.com>
2019-10-21 18:56:35 +0200

    "cleaned" xD
    uglyaf

--- admin/index.php
+++ admin/index.php
@@ -11,14 +11,12 @@
 <div class="table-responsive">
   <table class="table">
   <th>
- 
 <main role="main" class="container">
 <?php include "nav.php"; ?><br>
 <center>
 <a class="btn btn-sm btn-outline-secondary" href="_newUser.php" role="button">Neuer User</a>
 <a class="btn btn-sm btn-outline-secondary" href="<?=$urlMap ?>" target=_blank role="button">zur Map</a>
 <a class="btn btn-sm btn-outline-secondary" href="https://api.telegram.org/<?=$apitoken?>/exportChatInviteLink?chat_id=<?=$chatid?>" target=_blank role="button">Neuer Invitelink</a></center>
-
 <p>
 <div class="table-responsive-sm">
 	<table class="table">
@@ -34,12 +32,9 @@
 //Output any connection error
 if ($mysqli->connect_error) {
 	die('Error : ('. $mysqli->connect_errno .') '. $mysqli->connect_error);
-}
-					
+}			
 $query = "SELECT * FROM ".$tbl." ORDER BY endtime ASC";
-
 $result = $mysqli->query($query);
-
 while($row = $result->fetch_array()) { $paydate = $row["paydate"];
 $pd = new DateTime($paydate);
 $enddate = $row["endtime"];
@@ -57,11 +52,10 @@ $ed = new DateTime($enddate);?>
 <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
 <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
-     </div>
-	 	  </th>
-	  </table>
-
 </div>
-  </main>
+</th>
+</table>
+</div>
+</main>
 </body>
 </html>
